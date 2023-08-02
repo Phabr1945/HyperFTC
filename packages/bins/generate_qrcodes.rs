52ead32e609594629fad33c5f41a9d5cffdd89aa
AtomicGamer9523 <drunkenmonkey4987@gmail.com>
2023-08-02 12:30:58 -0700

    Pushed basic changes

---
+++ packages/bins/generate_qrcodes.rs
@@ -0,0 +1,34 @@
+use qrcode::*;
+
+use std::path::Path;
+use std::io::Write;
+use std::fs;
+
+const TEAM_NUMBER: &str = "20022";
+
+fn generate(value: String) -> AnyResult {
+    // Generating the QR code
+    let qr = QR::from_str(&value)?;
+    let svg = qr.image(QRImageType::SVG);
+
+    // Saving the QR code to a file
+    let name = format!("./.build/qrcodes/{}.svg", value);
+    let mut file = fs::File::create(name)?;
+    file.write_all(svg.as_bytes())?;
+    Ok(())
+}
+
+fn main() -> AnyResult {
+    // Creating the folder if needed
+    let path = Path::new("./.build/qrcodes");
+    if !path.exists() {
+        fs::create_dir(path)?;
+    }
+
+    // Generating the QR codes
+    generate(format!("{}-1", TEAM_NUMBER))?;
+    generate(format!("{}-2", TEAM_NUMBER))?;
+    generate(format!("{}-3", TEAM_NUMBER))?;
+
+    Ok(())
+}
---
+++ packages/qrcode/lib.rs
@@ -0,0 +1,89 @@
+﻿use qrcode_core::*;
+
+#[derive(Clone, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct QR(QrCode);
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+#[repr(transparent)]
+pub struct QRMatrix(Vec<Vec<bool>>);
+
+impl QR {
+    pub fn from_str(value: &str) -> AnyResult<Self> {
+        Ok(QR(QrCode::encode_text(value, QrCodeEcc::Low)?))
+    }
+
+    pub fn from_string(value: String) -> AnyResult<Self> {
+        Self::from_str(&value)
+    }
+
+    pub fn image(&self, image: QRImageType) -> String {
+        let matrix = self.matrix();
+        match image {
+            QRImageType::ASCII => matrix.ascii_image(),
+            QRImageType::SVG => matrix.svg_image(),
+        }
+    }
+
+    pub fn matrix(&self) -> QRMatrix {
+        let size = self.0.size();
+        let size_u = size as usize;
+        let mut rows = Vec::with_capacity(size_u);
+        for y in 0..size {
+            let mut row = Vec::with_capacity(size_u);
+            for x in 0..size {
+                row.push(self.0.get_module(x, y));
+            }
+            rows.push(row);
+        }
+        QRMatrix(rows)
+    }
+}
+
+pub type AnyResult<T=(),E=Box<dyn std::error::Error>> = Result<T,E>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum QRImageType {
+    ASCII,
+    SVG,
+}
+
+impl QRMatrix {
+    pub fn ascii_image(&self) -> String {
+        let rows: Vec<String> = self.0.iter().map(|row| {
+            row.iter().map(|&b| if b { "██" } else { "  " }).collect()
+        }).collect();
+        rows.join("\n")
+    }
+    pub fn svg_image(&self) -> String {
+        let size = self.0.len();
+        let mut svg = format!(
+            "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 {} {}\" shape-rendering=\"crispEdges\" width=\"{}\" height=\"{}\" >\n",
+            &size, &size, &size, &size
+        );
+        let rows = self.0.iter().enumerate().map(|(y, row)| {
+            let mut row_svg = String::new();
+            for (x, &b) in row.iter().enumerate() {
+                if b {
+                    row_svg.push_str(&format!("<rect x=\"{}\" y=\"{}\" width=\"1\" height=\"1\" fill=\"black\" />\n", x, y));
+                }
+            }
+            row_svg
+        }).collect::<Vec<String>>().join("");
+        svg.push_str(&rows);
+        svg.push_str("</svg>");
+        svg
+    }
+}
+
+impl std::fmt::Debug for QR {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.matrix())
+    }
+}
+
+impl std::fmt::Debug for QRMatrix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
+}
